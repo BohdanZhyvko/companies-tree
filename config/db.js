fe22cf0a2e609d5c4a8197757f40b6bfcb6db87c
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-06-18 01:35:28 +0300

    added work api using CRUD for work with mongodb

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://user1:user@ds125262.mlab.com:25262/companies"
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,82 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var db = require('../config/db.js');
+var assert = require('assert');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  mongo.connect(db.url, function(err, db) {
+    assert.equal(null, err);
+    var cursor = db.collection('companies').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      db.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
+
+router.post('/insert', function(req, res, next) {
+  var item = {
+    name: req.body.name,
+    estimatedEarnings: req.body.estimatedEarnings,
+    estimatedEarningsWithChild: req.body.estimatedEarningsWithChild,
+    parentId: req.body.parentId,
+    childId: req.body.childId
+  };
+
+  mongo.connect(db.url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('companies').insertOne(item, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item inserted');
+      db.close();
+    });
+  });
+
+  res.redirect('/');
+});
+
+router.post('/update', function(req, res, next) {
+  var item = {
+    name: req.body.name,
+    estimatedEarnings: req.body.estimatedEarnings,
+    estimatedEarningsWithChild: req.body.estimatedEarningsWithChild,
+    parentId: req.body.parentId,
+    childId: req.body.childId
+  };
+  var id = req.body.id;
+
+  mongo.connect(db.url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('companies').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item updated');
+      db.close();
+    });
+  });
+});
+
+router.post('/delete', function(req, res, next) {
+  var id = req.body.id;
+  
+  mongo.connect(db.url, function(err, db) {
+    assert.equal(null, err);
+    db.collection('companies').deleteOne({"_id": objectId(id)}, function(err, result) {
+      assert.equal(null, err);
+      console.log('Item deleted');
+      db.close();
+    });
+  });
 });
 
 module.exports = router;
