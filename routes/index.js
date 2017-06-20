7ac8d44cc5bcbb66d965e377dbf591ae0c52fc25
BohdanZhyvko <bohdan.zhyvko@gmail.com>
2017-06-20 13:06:32 +0300

    owerweiting parent and child when we delete data

--- routes/index.js
+++ routes/index.js
@@ -65,16 +65,51 @@ router.post('/update', function(req, res, next) {
     });
   });
 });
-
+// Delete method
 router.post('/delete', function(req, res, next) {
   var id = req.body.id;
-  
+
   mongo.connect(db.url, function(err, db) {
     assert.equal(null, err);
-    db.collection('companies').deleteOne({"_id": objectId(id)}, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item deleted');
-      db.close();
+    db.collection('companies').findOne({"_id": objectId(id)}, function(err, doc) {
+      var writeParentItem = {
+        childId: doc.childId
+      };
+      var writeChildItem = {
+        parentId: doc.parentId
+      };
+
+//Overwriting parent and child when we delete a data
+      if(doc.childId != '' && doc.parentId != ''){
+            var write = [writeParentItem, writeChildItem];
+            var writeId = [doc.parentId, doc.childId];
+            for(var i = 0; i < write.length; i++){
+              db.collection('companies').updateOne({"_id": objectId(writeId[i])},
+                {$set: write[i]}, function(err, result) {
+                assert.equal(null, err);
+                console.log('Item updated');
+            });
+            }
+        }else if (doc.parentId != '') {
+            db.collection('companies').updateOne({"_id": objectId(doc.parentId)},
+              {$set: {childId : ""}}, function(err, result) {
+              assert.equal(null, err);
+              console.log('Item updated');
+            });
+        }else if (doc.childId != ''){
+          db.collection('companies').updateOne({"_id": objectId(doc.childId)},
+          {$set: {parentId : ""}}, function(err, result) {
+            assert.equal(null, err);
+            console.log('Item updated');
+          });
+        }
+
+     });
+     //Delete data
+     db.collection('companies').deleteOne({"_id": objectId(id)}, function(err, result) {
+       assert.equal(null, err);
+       console.log('Item deleted');
+       db.close();
     });
   });
 });
